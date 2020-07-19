447aec244f07f4bb67287a9cc36d554bb452c85b
Sumit <agentsumit007@gmail.com>
2020-07-20 00:18:29 +0530

    update

---
+++ expense/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+</head>
+<body>
+    <?php
+        require "includes/nav.php"
+    ?>
+    <div id="banner-image">
+        <div class="container">
+            <div id="banner-content">
+                <h1>We help you control your budget</h1>
+                <a href="login.html" class="btn btn-info btn-lg active">Start Today</a>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="container-fluid">
+            <center>
+                Copyright © Lifestyle Store. All Rights Reserved” ​and ​“Contact Us: +91 90000 00000
+            </center>
+        </div>
+    </footer>
+</body>
+</html>  
